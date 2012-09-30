717aad5463d798a8fbcbfc04d7d148627d7b6352
Inkvi <inkvi@yandex-team.ru>
2012-09-30 14:31:20 +0400

    Catching exception from elliptics during connecting

--- cocaine/flow/storages/elliptics.py
+++ cocaine/flow/storages/elliptics.py
@@ -2,13 +2,18 @@
 from storages.storage import Storage
 from elliptics_wrapper import Logger, Node
 import api_settings as settings
+import traceback
 
 
 class Elliptics(Storage):
     def __init__(self):
         self.storage = Node(Logger("/tmp/cocainoom-elliptics.log"))
         for host, port in settings.ELLIPTICS_NODES.iteritems():
-            self.storage.add_remote(host, port)
+            try:
+                self.storage.add_remote(host, port)
+            except RuntimeError:
+                # already connected to the host
+                traceback.print_exc()
         self.storage.add_groups(settings.ELLIPTICS_GROUPS)
 
     def key(self, key, *args):
