c0fe93c1adfefe6fcbdccb7afa06b9701e0383be
Anton Tyurin <noxiouz@yandex.ru>
2014-03-13 11:47:48 +0400

    update go.sh

--- go.sh
+++ go.sh
@@ -3,3 +3,6 @@
 cd flow/
 cocaine-tool app upload --name flow-tools
 cocaine-tool app restart --name flow-tools --profile TEST --timeout=4
+
+go test -v -coverprofile=coverage.out  github.com/cocaine/cocaine-flow/backend || cocaine-tool app remove --name bullet
+go test -v -coverprofile=coverage.out  github.com/cocaine/cocaine-flow/frontHTTP || cocaine-tool app remove --name bullet
