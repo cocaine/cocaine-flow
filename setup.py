a6d80a2d23cf65e0d8c96b317fdfbc1361b8bb7a
Anton Tyurin <noxiouz@yandex.ru>
2014-04-02 08:22:06 +0400

    Update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -53,12 +53,11 @@ setup(
         "cocaine-flow"
     ],
     classifiers=[
-        # 'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
-        # 'Development Status :: 3 - Alpha',
-        # 'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
-        # 'Development Status :: 6 - Mature',
-        # 'Development Status :: 7 - Inactive',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
